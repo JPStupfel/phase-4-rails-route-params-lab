725c538a89bc4f836ea27289165ca18dcf0eed0f
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-26 15:47:02 -0500

    commit tree e3969c723789471a1ef1e50f70060a52afaebefc

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,26 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    # name = params[:name] ? params[:name].downcase
+     if params[:name] 
+      name = params[:name].capitalize
+    
+      student = Student.where(first_name: name).or(Student.where(last_name: name))
+      render json: student
+     else
+
+    # byebug
     render json: students
+     end
   end
+
+  def show
+    student = Student.find params[:id]
+    render json: student
+  end
+
+  # def search
+  #   byebug
+  # end
 
 end
